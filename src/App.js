deca0a388455c32eeeebc3fadd756726fd00973c
dawon <lyumilove9@gmail.com>
2021-07-13 20:17:35 +0900

    component와 propTypes 연습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+
+const oniLike = [
+  {
+    id: 1,
+    name: "gosomi",
+    rating: 3
+  },
+  {
+    id: 2,
+    name: "ddangkong",
+    rating: 5
+  },
+  {
+    id: 3,
+    name: "BR",
+    rating: 4.9
+  },
+  {
+    id: 4,
+    name: "hs",
+    rating: 5
+  }
+]
+
+function Oni({ name, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+    </div>
+  );
+}
+
+Oni.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {oniLike.map(fav => (
+        <Oni key={fav.id} name={fav.name} rating={fav.rating} />
+      ))}
     </div>
   );
 }
