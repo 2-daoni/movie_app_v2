1e8cb1f106545fd2af73433425158504790c564d
dawon <lyumilove9@gmail.com>
2021-07-15 21:33:37 +0900

    Updates

---
+++ static/js/main.881cc5fc.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmovie_app_v2=this.webpackJsonpmovie_app_v2||[]).push([[0],{27:function(t,e,n){"use strict";n.r(e);var c=n(0),r=n.n(c),a=n(14),o=n.n(a),s=n(15),i=n(1),p=n(6);var j=function(){return Object(p.jsx)("span",{children:"About this page: "})};var u=function(){return Object(p.jsx)(s.a,{children:Object(p.jsx)(i.a,{path:"/about",component:j})})};o.a.render(Object(p.jsx)(r.a.StrictMode,{children:Object(p.jsx)(u,{})}),document.getElementById("root"))}},[[27,1,2]]]);
+//# sourceMappingURL=main.881cc5fc.chunk.js.map
