7760bc3d0d2e61cf04e32217c23a69e29035ea79
dawon <lyumilove9@gmail.com>
2021-07-15 22:00:02 +0900

    router제작

--- src/App.js
+++ src/App.js
@@ -1,53 +1,13 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import {HashRouter, Route} from "react-router-dom";
+import Home from "./routes/home";
+import About from "./routes/about";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  getMovies = async () => {
-    //axios.get이 완료되기까지 시간이 필요함으로 await
-    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({ movies, isLoading: false });
-  };
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-              <span className="loader_text">Loading...</span>
-          </div> 
-        ) : (
-          <div className="movies">
-            {movies.map(movie =>{
-              return(
-                <Movie
-                key = {movie.id}
-                id={movie.id}
-                title={movie.title}
-                year={movie.year}
-                genres={movie.genres}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-              />
-              )
-              
-            })}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App(){
+  return <HashRouter>
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About} />
+  </HashRouter>
 }
 
 export default App;
